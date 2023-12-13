b5c816bd8ad24bc8845882214eef5632ffaa3a24
jingyan <jingyan.dong420@gmail.com>
2023-12-13 17:56:32 +0100

    Create the app project with file structure: create the homeview with local database and router, add basic styling in css.

---
+++ file structure.md
@@ -0,0 +1,25 @@
+my-vue-app/
+|-- node_modules/
+|-- public/
+|   |-- index.html
+|-- src/
+|   |-- assets/
+|   |   |-- logo.png
+|   |   |-- style.css
+|   |-- components/
+|   |   |-- CreateHouseForm.vue
+|   |   |-- HouseList.vue
+|   |   |-- SearchComponent.vue
+|   |-- views/
+|   |   |-- Home.vue
+|   |   |-- About.vue
+|   |   |-- HouseDetail.vue
+|   |-- router/
+|   |   |-- index.js
+|   |-- services/
+|   |   |-- api.js
+|   |-- App.vue
+|   |-- main.js
+|-- .gitignore
+|-- package.json
+|-- README.md
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,66 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+      <div class="body-container">
+        <div class="title">
+          <span>House</span>
+          <button>+ Create New</button>      
+        </div>
+
+        <div class="sorting">
+          <input type="text" placeholder="Search...">
+          <div>
+            <button>Price Filter</button>
+            <button>Size Filter</button>
+          </div>
+        </div>
+
+        <!--This is houses container-->
+        <div class="items-container">
+          <div class="items" v-for="item in houses" :key="item.id">
+            <div class="item">
+              <img class="item-img" src="../../public/images/img_placeholder_house@3x.png" :alt="item.id" />
+              <div class="item-info">
+                <h5>{{item.location.street}} {{item.location.houseNumber}}</h5>
+                <p>€ {{item.price}}</p>
+                <p>{{item.location.zip}} {{item.location.city}}</p>
+                <div class="item-info-bbg">
+                  <div class="item-info-bbg-bed">
+                    <img class="img-small" src="../../public/images/ic_bed@3x.png"/>
+                    <p>{{item.rooms.bedrooms}}</p>
+                  </div>
+                  <div class="item-info-bbg-bath">
+                      <img class="img-small" src="../../public/images/ic_bath@3x.png" />
+                      <p>{{item.rooms.bathrooms}}</p>
+                  </div>
+                  <div class="item-info-bbg-size">
+                      <img class="img-small" src="../../public/images/ic_size@3x.png" />
+                      <p>{{item.size}} m2</p>
+                  </div>
+                </div>
+              </div>
+              <div class="item-edits">
+                    <!--change here, can use a container, in the tag write v-on:click="javascript function/method" or @click-->
+                  <button>edit</button>
+                    <button>delete</button>
+              </div>
+              </div>
+            </div>
+        </div>
+      </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+//import HelloWorld from "@/components/HelloWorld.vue";
+import houses from "@/houses.json"
 
 export default {
   name: "HomeView",
   components: {
-    HelloWorld,
   },
+  data(){
+    return {
+      houses: houses.houses
+    }
+  }
 };
 </script>
